07ca324a3dbd86ea8e978d73276760c167c976a9
m-pavlovic <marija.pavlovic19@gmail.com>
2023-10-05 10:58:38 +0200

    save to one file

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator extends JFrame implements ActionListener {
     private static int invoiceCounter = 1;
     static int searchID;
     static String searchName;
-    static HashMap<String, String> invoiceMap = new HashMap<>();
+    static LinkedHashMap<String, String> invoiceMap = new LinkedHashMap<>();
 
 
     Calculator() {
@@ -97,7 +97,8 @@ public class Calculator extends JFrame implements ActionListener {
         invoiceMap.put("Invoice ID", String.valueOf(invoiceCounter));
         invoiceMap.put("------------------------------------------------------------\nCustomer", ViewPanel.customerInfo());
         invoiceMap.put("------------------------------------------------------------\nOFFER", Offer.getInvoiceText());
-        saveToFile();
+        invoiceMap.put("******************************************************", "");
+        //saveToFile();
         saveToOneFile();
 
     }
@@ -113,24 +114,24 @@ public class Calculator extends JFrame implements ActionListener {
         }
     }
 
-    /**
-     * calls the checkIfFolderExists method and saves the invoice to a file
-     */
-
-    private void saveToFile() {
-        checkIfFolderExists();
-        try {
-            File file = new File("invoices/invoice" + invoiceCounter + ".txt");
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bw = new BufferedWriter(fw);
-            for (Map.Entry<String, String> entry : invoiceMap.entrySet()) {
-                bw.write(entry.getKey() + ": " + entry.getValue() + "\n");
-            }
-            bw.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    /**
+//     * calls the checkIfFolderExists method and saves the invoice to a file
+//     */
+//
+//    private void saveToFile() {
+//        checkIfFolderExists();
+//        try {
+//            File file = new File("invoices/invoice" + invoiceCounter + ".txt");
+//            FileWriter fw = new FileWriter(file);
+//            BufferedWriter bw = new BufferedWriter(fw);
+//            for (Map.Entry<String, String> entry : invoiceMap.entrySet()) {
+//                bw.write(entry.getKey() + ": " + entry.getValue() + "\n");
+//            }
+//            bw.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     /**
      * Saves all invoices to one file
@@ -187,17 +188,96 @@ public class Calculator extends JFrame implements ActionListener {
     }
 
 
-
     /**
      * Searches for an invoice by ID
-     * @param searchID
+     *
+     * @param searchID The ID to search for
+     */
+    private static void searchByID(int searchID) {
+        File file = new File("invoices/invoice.txt");
+
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(file)) {
+                StringBuilder invoiceText = new StringBuilder();
+                boolean found = false;
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.contains("Invoice ID: " + searchID)) {
+                        found = true;
+                    }
+
+                    if (found) {
+                        invoiceText.append(line).append("\n");
+                        if (line.contains("******************************************************")) {
+                            break;
+                        }
+                    }
+                }
+
+                if (found) {
+                    ViewPanel.setTextOnTextArea(invoiceText.toString());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Could not load file!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+
+//    private static void searchByID(int searchID) {
+//        File file = new File("invoices/invoice" + searchID + ".txt");
+//        if (file.exists()) {
+//            try {
+//                Desktop.getDesktop().open(file);
+//            } catch (IOException e) {
+//                JOptionPane.showMessageDialog(null, "Could not load file!", "Error", JOptionPane.ERROR_MESSAGE);
+//            }
+//        } else {
+//            JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
+//        }
+//
+//    }
+
+    /**
+     * Searches for an invoice by Name
+     *
+     * @param searchName
      */
 
-    private static void searchByID(int searchID) {
-        File file = new File("invoices/invoice" + searchID + ".txt");
+    private static void searchByName(String searchName) {
+        File file = new File("invoices/invoice.txt");
+
         if (file.exists()) {
-            try {
-                Desktop.getDesktop().open(file);
+            try (Scanner scanner = new Scanner(file)) {
+                StringBuilder invoiceText = new StringBuilder();
+                boolean found = false;
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine().toUpperCase();
+                    if (line.contains(searchName.toUpperCase())) {
+                        found = true;
+                        invoiceText.append(line).append("\n");
+
+                        while (scanner.hasNextLine()) {
+                            String nextLine = scanner.nextLine();
+                            invoiceText.append(nextLine).append("\n");
+                            if (nextLine.contains("******************************************************")) {
+                                break;
+                            }
+                        }
+                        ViewPanel.setTextOnTextArea(invoiceText.toString());
+                        invoiceText.setLength(0);
+                    }
+                }
+
+                if (!found) {
+                    JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
+                }
             } catch (IOException e) {
                 JOptionPane.showMessageDialog(null, "Could not load file!", "Error", JOptionPane.ERROR_MESSAGE);
             }
@@ -205,38 +285,33 @@ public class Calculator extends JFrame implements ActionListener {
             JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
         }
 
-    }
 
-    /**
-     * Searches for an invoice by Name
-     * @param searchName
-     */
-
-    private static void searchByName(String searchName) {
-        File folder = new File("invoices");
-        File[] listOfFiles = folder.listFiles();
-
-        boolean found = false;
-
-        for (File file : listOfFiles != null ? listOfFiles : new File[0]) {
-            if (!file.isFile()) continue;
-
-            try (Scanner scanner = new Scanner(file)) {
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine().toLowerCase();
-                    if (line.contains(searchName.toLowerCase())) {
-                        Desktop.getDesktop().open(file);
-                        found = true;
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                JOptionPane.showMessageDialog(null, "Could not load file!", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-
-        if (!found) {
-            JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
-        }
+//    private static void searchByName(String searchName) {
+//        File folder = new File("invoices");
+//        File[] listOfFiles = folder.listFiles();
+//
+//        boolean found = false;
+//
+//        for (File file : listOfFiles != null ? listOfFiles : new File[0]) {
+//            if (!file.isFile()) continue;
+//
+//            try (Scanner scanner = new Scanner(file)) {
+//                while (scanner.hasNextLine()) {
+//                    String line = scanner.nextLine().toLowerCase();
+//                    if (line.contains(searchName.toLowerCase())) {
+//                        Desktop.getDesktop().open(file);
+//                        found = true;
+//                        break;
+//                    }
+//                }
+//            } catch (IOException e) {
+//                JOptionPane.showMessageDialog(null, "Could not load file!", "Error", JOptionPane.ERROR_MESSAGE);
+//            }
+//        }
+//
+//        if (!found) {
+//            JOptionPane.showMessageDialog(null, "Invoice does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
+//        }
+//    }
     }
 }
